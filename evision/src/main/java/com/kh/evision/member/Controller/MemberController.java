669eae075dbb49c020ec0f61f7979ee59c58f418
parkhj86 <parkhj86@naver.com>
2025-11-17 17:23:36 +0900

    memberDTO

---
+++ evision/src/main/java/com/kh/evision/member/Controller/MemberController.java
@@ -0,0 +1,5 @@
+package com.kh.evision.member.Controller;
+
+public class MemberController {
+
+}
---
+++ evision/src/main/java/com/kh/evision/member/model/dto/MemberDTO.java
@@ -0,0 +1,12 @@
+package com.kh.evision.member.model.dto;
+
+public class MemberDTO {
+	private String memberName;
+	private String memberId;
+	private String memberPwd;
+	private String nickname;
+	private String address;
+	private String phone;
+	private String email;
+	private String roleStatus;
+}
