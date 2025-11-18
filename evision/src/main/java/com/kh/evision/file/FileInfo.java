e7debe586ffa1dcd9dad8df35784d3e7b7cf1492
s2selyn <s2selyn@gmail.com>
2025-11-18 10:29:53 +0900

    file service ver.1

--- evision/src/main/java/com/kh/evision/file/FileInfo.java
+++ evision/src/main/java/com/kh/evision/file/FileInfo.java
@@ -1,18 +1,18 @@
 package com.kh.evision.file;
 
+import java.nio.file.Path;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class FileInfo {
 	
 	private String originName;
 	private String changeName;
-	private String filePath;
+	private Path filePath;
 
 }
--- evision/src/main/java/com/kh/evision/file/FileService.java
+++ evision/src/main/java/com/kh/evision/file/FileService.java
@@ -1,12 +1,17 @@
 package com.kh.evision.file;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.evision.exception.FileUploadFailureException;
 
 @Service
 public class FileService {
@@ -14,34 +19,57 @@ public class FileService {
 	// 파일 경로 필드
 	private final Path fileLocation;
 	
-	// 파일 경로 생성자에 서버의 업로드 경로 추가
+	// 생성자에 서버로의 파일 업로드 경로 추가
 	public FileService() {
 		this.fileLocation = Paths.get("uploads").toAbsolutePath().normalize();
 	}
 	
 	// 파일 업로드 메소드
-	public String store(MultipartFile file) {
+	public FileInfo store(MultipartFile file) {
 		
-		String originalFilename = file.getOriginalFilename();
-		String changedFileName = getChangedFileName(originalFilename);
+		// 파일명 관련작업
+		String originalFileName = file.getOriginalFilename();
+		String changedFileName = getChangedFileName(originalFileName);
 		
+		// 파일 경로 관련작업
 		Path targetLocation = this.fileLocation.resolve(changedFileName);
+		FileInfo fileInfo = new FileInfo(originalFileName, changedFileName, targetLocation);
+		
+		// 업로드 시도
+		try {
+			
+			Files.copy(file.getInputStream(),
+					   targetLocation,
+					   StandardCopyOption.REPLACE_EXISTING);
+			return fileInfo;
+			
+		// 예외처리
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			throw new FileUploadFailureException("파일 업로드에 실패했습니다.");
+			
+		}
 		
 	}
 	
 	// 파일 이름 변경 메소드
 	private String getChangedFileName(String originalFileName) {
 		
+		// 접두규칙
 		StringBuilder sb = new StringBuilder();
 		sb.append("Evision_");
 		
+		// 년월일시분초
 		String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 		sb.append(currentTime);
 		sb.append("_");
 		
+		// 임의의 수 생성
 		int num = (int)(Math.random() * 900) + 100;
 		sb.append(num);
 		
+		// 원본 확장자 추가
 		String ext = originalFileName.substring(originalFileName.lastIndexOf("."));
 		sb.append(ext);
 		
