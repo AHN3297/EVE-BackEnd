7a54d92194439747f9b5296d636def3486ce9362
s2selyn <s2selyn@gmail.com>
2025-11-18 14:51:01 +0900

    setup car controller

--- evision/src/main/java/com/kh/evision/car/controller/CarController.java
+++ evision/src/main/java/com/kh/evision/car/controller/CarController.java
@@ -1,10 +1,17 @@
 package com.kh.evision.car.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.evision.car.model.dto.CarDTO;
 import com.kh.evision.car.model.service.CarService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -14,7 +21,21 @@ public class CarController {
 	
 	private final CarService carService;
 	
-	// 차량 등록
+	// 차량 등록 -> 관리자/운영자용 기능, 이미지첨부, 파일첨부
+	@PostMapping
+	public ResponseEntity<?> saveCar(@Valid CarDTO car,
+			@RequestParam(name="file", required=false) MultipartFile file
+			// , @AuthenticationPrincipal CustomUserDetails userDetails
+			) {
+		
+		carService.saveCar(car,
+				file
+				// , userDetails
+				);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+		
+	}
 	
 	// 차량 목록 조회
 	
---
+++ evision/src/main/java/com/kh/evision/car/model/dto/CarDTO.java
@@ -0,0 +1,32 @@
+package com.kh.evision.car.model.dto;
+
+import java.sql.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CarDTO {
+	
+	private Long carNo;
+	
+	@NotBlank
+	private String carName;
+	
+	private String carPlate;
+	private int maxPassenger;
+	private String color;
+	private String status;
+	private Date registerDate;
+	private String carLocation;
+	private String carBrand;
+
+}
