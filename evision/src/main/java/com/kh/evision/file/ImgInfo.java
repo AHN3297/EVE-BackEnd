9110b74459261111787fb83849844d878e215a6d
s2selyn <s2selyn@gmail.com>
2025-11-20 10:29:56 +0900

    setup ImgInfo class and edit yml

---
+++ evision/src/main/java/com/kh/evision/file/ImgInfo.java
@@ -0,0 +1,18 @@
+package com.kh.evision.file;
+
+import java.nio.file.Path;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ImgInfo {
+	
+	private String originName;
+	private String changeName;
+	private Path filePath;
+
+}
